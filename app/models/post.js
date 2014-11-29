2d611025e21ceac1c8f38a957bc32948276ae425
Corey Wilson <phpuser@gmail.com>
2014-11-29 14:58:02 -0500

    added post attributes to the model

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
+var attr = DS.attr;
 
 export default DS.Model.extend({
-  
+  title: attr('string'),
+  url: attr('string'),
+  created_at: attr('date'),
+  updated_at: attr('date')
 });
