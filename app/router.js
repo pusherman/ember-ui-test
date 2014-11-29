94e34864d83162bcb8eb516d8dd76a7d8f9425a5
Corey Wilson <phpuser@gmail.com>
2014-11-29 12:43:30 -0500

    make / posts view

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('posts', function() {
+  this.resource('posts', {path: '/'},  function() {
     this.route('new');
   });
 });
