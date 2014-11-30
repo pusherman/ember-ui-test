6954f728195ef288241dc3ffb999d21b83fddb37
Corey Wilson <phpuser@gmail.com>
2014-11-30 13:15:10 -0500

    ember g helper fuzzy-date

---
+++ tests/unit/helpers/fuzzy-date-test.js
@@ -0,0 +1,11 @@
+import {
+  fuzzyDate
+} from 'fyibn-ui/helpers/fuzzy-date';
+
+module('FuzzyDateHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = fuzzyDate(42);
+  ok(result);
+});
